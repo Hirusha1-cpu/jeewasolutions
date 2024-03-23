3dc2ef184da1dcd36610d8199435ad8bfb51ebc8
Hirusha1-cpu <hirushafernando121@gmail.com>
2024-03-23 18:36:24 +0530

    grn

--- src/main/resources/static/supplyChainControllers/grnController.js
+++ src/main/resources/static/supplyChainControllers/grnController.js
@@ -1,1 +1,11 @@
+window.addEventListener('load', () => {
+    refreshGrnForm();
+    // refreshSupplyTable();
 
+})
+
+const refreshGrnForm = () => {
+    grn = new Object();
+    purchaseOrders = ajaxGetRequest("/purchase/getlist")
+    fillDataIntoSelect(selectPurchCategory,"Select Category",categories, 'name')
+}
