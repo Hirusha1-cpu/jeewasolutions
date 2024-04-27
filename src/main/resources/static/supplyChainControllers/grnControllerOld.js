e533c9085c0e441ed68b21382195985a6174cb80
Hirusha1-cpu <hirushafernando121@gmail.com>
2024-04-27 14:34:24 +0530

    ss

--- src/main/resources/static/supplyChainControllers/grnControllerOld.js
+++ src/main/resources/static/supplyChainControllers/grnControllerOld.js
@@ -17,10 +17,10 @@ const refreshGrnForm = () => {
     // grn.grnHasCategory.availableitems_id = new Array();
     // myArray = []
 
-    
+
     categories = ajaxGetRequest("/category/getlist")
     fillDataIntoSelect(selectCategories, "Select Category", categories, 'name')
-    
+
     suppliers = ajaxGetRequest("/supplier/getlist")
     fillDataIntoSelect(selectSuppliersList, "Select Supplier", suppliers, 'name')
     console.log(suppliers);
@@ -29,46 +29,71 @@ const refreshGrnForm = () => {
     // purchaseOrdersList = ajaxGetRequest("/purchase/getporeds")
 }
 
-const refreshGrnTable = ()=>{
+const refreshGrnTable = () => {
     grnList = ajaxGetRequest('/grn/getlist')
     const displayProperties = [
         { property: getGrnItemCategory, dataType: 'function' },
-        { property: getGrnItemBrand, dataType: 'function' },
+        // { property: getGrnItemBrand, dataType: 'function' },
         { property: getGrnsItemName, dataType: 'function' },
         { property: getGrnsItemQty, dataType: 'function' },
         { property: getGrnsItemDiscount, dataType: 'function' },
         { property: getGrnsItemSupplier, dataType: 'function' },
+        { property: getGrnsItemSupplierInvoice, dataType: 'function' },
         { property: getGrnsItemTotal, dataType: 'function' },
     ]
- 
+
 
     fillDataIntoTable(grnTab, grnList, displayProperties, refillGrnBtn, updateEmployeeBtn, deleteEmployeeBtn, true, null)
 
 }
 const getGrnItemCategory = (rowObject) => {
-    let GrnItemCategory  = '';
-    rowOb.purchaseHasCategory.forEach(element => {
-        purchCategory = purchCategory + "<p class = 'working-status'>" + element.category_id.name + "</p>"
+    let GrnItemCategory = '';
+    rowObject.grnHasCategory.forEach(element => {
+        GrnItemCategory = GrnItemCategory + "<p class = 'working-status'>" + element.category_id.name + "</p>"
     })
-    return purchCate.purchaseHasCategory
+    return GrnItemCategory
 }
-const getGrnItemBrand = (rowObject) => {
-}
+
 const getGrnsItemName = (rowObject) => {
+    let GrnItemName = '';
+    rowObject.grnHasCategory.forEach(element => {
+        GrnItemName = GrnItemName + "<p class = 'working-status'>" + element.itemname + "</p>"
+    })
+    return GrnItemName
 }
 const getGrnsItemQty = (rowObject) => {
+    let GrnItemCategory = '';
+    rowObject.grnHasCategory.forEach(element => {
+        GrnItemCategory = GrnItemCategory + "<p class = 'working-status'>" + element.qty + "</p>"
+    })
+    return GrnItemCategory
 }
 const getGrnsItemDiscount = (rowObject) => {
+    let GrnItemDiscount = '';
+    rowObject.grnHasCategory.forEach(element => {
+        GrnItemDiscount = GrnItemDiscount + "<p class = 'working-status'>" + element.discount + "</p>"
+    })
+    return GrnItemDiscount
 }
 const getGrnsItemSupplier = (rowObject) => {
+    return "<p class = 'working-status'>" + rowObject.purchase_id.supplier_id.name + "</p>"
+
+}
+const getGrnsItemSupplierInvoice = (rowObject) => {
+    return "<p class = 'working-status'>" + rowObject.supplierinvoiceno + "</p>"
+
 }
 const getGrnsItemTotal = (rowObject) => {
+    let GrnItemTotal = '';
+    rowObject.grnHasCategory.forEach(element => {
+
+        GrnItemTotal = GrnItemTotal + "<p class = 'working-status'>" + (element.item_price) * (element.qty) + "</p>"
+    })
+    return GrnItemTotal
 }
 
 
-const refillGrnBtn = (rowObject) => {
-    console.log("clicked purchase order");
-}
+
 const updateEmployeeBtn = (rowObject) => {
     console.log("clicked delete purchase order");
 }
@@ -76,17 +101,132 @@ const deleteEmployeeBtn = (rowObject) => {
     console.log("clicked send purchase order");
 }
 
-const porderLists = (item) =>{
+const porderLists = (item) => {
     // console.log(item);
     console.log(JSON.parse(selectSuppliersList.value).id);
-    purchaseOrdersList = ajaxGetRequest("/purchase/getpurchasesupplier/"+JSON.parse(selectSuppliersList.value).id)
+    purchaseOrdersList = ajaxGetRequest("/purchase/getpurchasesupplier/" + JSON.parse(selectSuppliersList.value).id)
     console.log(purchaseOrdersList);
     // purchaseOrdersList = ajaxGetRequest("/purchase/getlist")
     // purchaseOrdersList = ajaxGetRequest("/purchase/getpurchasesupplier/"+ (selectSuppliersList.value).id)
-    fillDataIntoSelectMulProp(selectPurchaseOrder1, "Select Purchase Order", purchaseOrdersList, 'purchase_code', "purchaseHasCategory",)
+    fillDataIntoSelect(selectPurchaseOrder1, "Select Purchase Order", purchaseOrdersList, 'purchase_code')
 
 }
+const refillGrnBtn = (item) => {
+    grn = JSON.parse(JSON.stringify(item));
+    oldgrn = JSON.parse(JSON.stringify(item));
+    console.log(grn);
+    console.log("clicked purchase order");
+    if (grn.grnHasCategory.length > 1) {
 
+        const inputNo = prompt("Enter a value for purchase.purchaseHasCategory[0]:");
+        const inputValue = parseInt(inputNo)
+        inputPurchaseQuantity.value = grn.grnHasCategory[inputValue].qty
+        inputPurchaseItemPrice.value = grn.grnHasCategory[inputValue].item_price
+        inputPurchaseLinePrice.value = grn.grnHasCategory[inputValue].lineprice
+        inputPurchaseDiscount.value = grn.grnHasCategory[inputValue].discount
+    }else{
+        inputPurchaseQuantity.value = grn.grnHasCategory[0].qty
+        inputPurchaseItemPrice.value = grn.grnHasCategory[0].item_price
+        inputPurchaseLinePrice.value = grn.grnHasCategory[0].lineprice
+        inputPurchaseDiscount.value = grn.grnHasCategory[0].discount
+    }
+    // console.log(inputValue);
+    fillDataIntoSelect(selectSuppliersList, "Select Supplier", suppliers, 'name', grn.purchase_id.supplier_id.name);
+    purchaseOrdersList1 = ajaxGetRequest("/purchase/getpurchasesupplier/" + JSON.parse(grn.purchase_id.supplier_id.id))
+    fillDataIntoSelect(selectPurchaseOrder1, "Select Purchase Order", purchaseOrdersList1, 'purchase_code', grn.purchase_id.purchase_code)
+
+    console.log(inputPurchaseQuantity.value);
+    const inputSerialNoDiv = document.querySelector("#inputSerialNoDiv");
+
+    inputSerialNoDiv.innerHTML = ""
+    const qty = parseInt(document.querySelector(".inputPurchaseQuantity").value)
+    const serialNumbers = []; // Array to store serial number objects
+
+    if (grn.grnHasCategory.length > 1) {
+
+        const inputNo = prompt("Enter a value for purchase.purchaseHasCategory[0]:");
+        const inputValue = parseInt(inputNo)
+        for (let id = 0; id < qty; id++) {
+            const div = document.createElement("div");
+            div.className = "col-3 form-floating p-1";
+    
+            const label = document.createElement("label");
+            label.setAttribute("for", "floatingInput");
+            label.innerText = "Serial No " + id;
+    
+            const input = document.createElement("input");
+            input.type = "text";
+            input.className = "form-control";
+            input.id = "txtqty" + id;
+    
+            input.value = grn.grnHasCategory[inputValue].serialNumbers[id].serialno
+    
+            // Add event listener to each input field
+            input.addEventListener("input", () => {
+                updateSerialNo(id, input.value);
+            });
+    
+            div.appendChild(label);
+            div.appendChild(input);
+            inputSerialNoDiv.appendChild(div);
+        }
+    }else{
+
+        for (let id = 0; id < qty; id++) {
+            const div = document.createElement("div");
+            div.className = "col-3 form-floating p-1";
+    
+            const label = document.createElement("label");
+            label.setAttribute("for", "floatingInput");
+            label.innerText = "Serial No " + (id+1);
+    
+            const input = document.createElement("input");
+            input.type = "text";
+            input.className = "form-control";
+            input.id = "txtqty" + id;
+    
+            input.value = grn.grnHasCategory[0].serialNumbers[id].serialno
+    
+            // Add event listener to each input field
+            input.addEventListener("input", () => {
+                updateSerialNo(id, input.value);
+            });
+    
+            div.appendChild(label);
+            div.appendChild(input);
+            inputSerialNoDiv.appendChild(div);
+        }
+    }
+
+    console.log("serialNo", serialNo);
+    for (let id in serialNo) {
+        serialNumbers.push({ serialno: serialNo[id] });
+    }
+
+    // return serialNumbers;
+
+    const updateSerialNo = (id, value) => {
+        serialNo[id] = value;
+        console.log("serialNo", serialNo);
+    }
+
+
+    const table = document.getElementById("empTable");
+    const form = document.getElementById("empForm");
+
+    // Animate table disappearance
+    table.style.opacity = 1; // Ensure opacity is initially 1
+    table.style.transition = "opacity 1.5s ease-out";
+    table.style.display = "none"; // Trigger the animation
+
+    // Delay form appearance slightly
+    setTimeout(function () {
+        form.style.opacity = 0;
+        form.style.display = "block";
+        form.style.transition = "opacity 1.5s ease-in";
+        form.style.opacity = 1; // Gradually fade in
+    }, 100); // Adjust the delay as needed    
+}
 // const getPurchBySuppliers = () =>{
 //     purchaseOrdersList = ajaxGetRequest("/purchase/getpurchasesupplier/1")
 //     fillDataIntoSelect(selectPurchaseOrder1, "Select Purchase Order", purchaseOrdersList, 'id')
@@ -203,25 +343,25 @@ const getGrnPurchaseOrderStatus = (rowOb) => {
 
 const getPurchaseCode = (rowObject) => {
     console.log("rowObject", rowObject);
-    return rowObject.purchase_code ?? "-"; 
+    return rowObject.purchase_code ?? "-";
 }
 const getItemName = (rowObject) => {
 
-    return rowObject.itemname ?? "-"; 
+    return rowObject.itemname ?? "-";
 }
 const getItemPrice = (rowObject) => {
-    return rowObject.itemprice ?? "-"; 
+    return rowObject.itemprice ?? "-";
 }
 const getItemQty = (rowObject) => {
     return rowObject.qty ?? "-";
 }
 const getLinePrice = (rowObject) => {
-    return rowObject.lineprice ?? "-"; 
+    return rowObject.lineprice ?? "-";
 }
 
 const getSupplierName = (rowObject) => {
     console.log("supplier_id", rowObject.supplier_id);
-    return rowObject.supplier_id?.name ?? "-"; 
+    return rowObject.supplier_id?.name ?? "-";
 }
 const getPurchaseOrderStatus = (rowObject) => {
 
@@ -250,7 +390,7 @@ const checkBoxButton = (rowObject) => {
     inputPurchaseLinePrice.value = rowObject.lineprice
     purchaseOId.innerHTML = rowObject.id
     grnHasItems.category_id = rowObject.category_id
-    
+
 }
 
 const addPurchaseOrderItemToTable = () => {
@@ -269,7 +409,7 @@ const generateLinePrice = () => {
     grnHasItems.lineprice = linePrice;
     inputPurchaseLinePrice.value = linePrice;
 }
-const generateSerialNumberList = () =>{
+const generateSerialNumberList = () => {
     const inputSerialNoDiv = document.querySelector("#inputSerialNoDiv");
 
     inputSerialNoDiv.innerHTML = ""
@@ -298,7 +438,7 @@ const generateSerialNumberList = () =>{
         div.appendChild(input);
         inputSerialNoDiv.appendChild(div);
     }
-    console.log("serialNo",serialNo);
+    console.log("serialNo", serialNo);
     for (let id in serialNo) {
         serialNumbers.push({ serialno: serialNo[id] });
     }
@@ -357,6 +497,22 @@ const addGrnMain = () => {
     inputGrnTotalAmount.value = ""
     inputGrnDiscount.value = ""
     inputGrnNetAmount.value = ""
+
+    const table = document.getElementById("empTable");
+    const form = document.getElementById("empForm");
+
+    // Animate table disappearance
+    form.style.opacity = 1; // Ensure opacity is initially 1
+    form.style.transition = "opacity 1.5s ease-out";
+    form.style.display = "none"; // Trigger the animation
+
+    // Delay form appearance slightly
+    setTimeout(function () {
+        table.style.opacity = 0;
+        table.style.display = "block";
+        table.style.transition = "opacity 1.5s ease-in";
+        table.style.opacity = 1; // Gradually fade in
+    }, 100); // Adjust the delay as needed
 
 }
 
